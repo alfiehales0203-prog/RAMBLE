67c3f53d415972fb3894e51d4482b265f329d4de
ALFIE <alfiehales0203@gmail.com>
2026-02-10 12:44:12 +0000

    Initial Commit

---
+++ RAMBLE Watch App/RAMBLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  RAMBLEApp.swift
+//  RAMBLE Watch App
+//
+//  Created by Alfie Hales on 10/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct RAMBLE_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RAMBLE/RAMBLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  RAMBLEApp.swift
+//  RAMBLE
+//
+//  Created by Alfie Hales on 10/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct RAMBLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
